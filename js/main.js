602c4ae647ab456a5db9b8f909e08d024b0263e7
HazelicEyes <henriquepromail9863@gmail.com>
2020-12-14 04:57:14 -0300

    initial commit, add html file and main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Yuniheim";
+var n1 = 19;
+var n2 = 10;
+var frase = "tem um gosto meio roxo"
+//alert(" bem vindo " + nome + " vc tem " + idade + " anos");
+//alert (idade + idade2)
+
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace ("roxo", "azul"))
